7e80b64b7a197e0518e2420e3ab035f58f21090c
Robert Li <bobli2011@gmail.com>
2019-11-18 12:58:02 -0500

    added basic login/registration pages

--- RideShare/src/app/app.module.ts
+++ RideShare/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';	
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routing } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { NavbarOuterComponent } from './navbar-outer/navbar-outer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    NavbarOuterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing,
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ RideShare/src/app/login/login.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  title = 'Welcome to RideShare';
+  username: ""
+  password: ""
+  response: string;
+
+  /*
+  elemResources = document.getElementById('resources-link');
+  elemForum = document.getElementById('forum-link');
+  elemQuiz = document.getElementById('quiz-link');
+  elemHome = document.getElementById('home-link');
+  elemProfile = document.getElementById('profile');
+  elemSearch = document.getElementById('search'); 
+  elemLogout = document.getElementById('logout');
+  */
+
+  onRegister(): void {
+    this.router.navigate(['registration']);
+  }
+  onLogin(): void {
+    /*
+    let url = 'http://localhost:8080/LightHouse/login';
+    let result = this.http.post<ControllerResponse>(url, {
+      username: this.username,
+      password: this.password
+    }).subscribe(cr => {
+      if (cr.response === "success") {
+        console.log("Response" + cr.response);
+        sessionStorage.setItem(
+          'user', this.username
+        ); 
+        console.log("User: " + sessionStorage.getItem('user'));
+        this.router.navigate(['home']);
+      } else {
+        console.log("Response" + cr.response);
+        this.response = cr.response;
+        //alert("Authentication failed.");
+      }
+    });
+    */
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
+
+
+  ) { }
+
+  ngOnInit() {
+    /*
+    sessionStorage.setItem('token', '');
+    this.elemResources.style.visibility = "hidden";
+    this.elemForum.style.visibility = "hidden";
+    this.elemQuiz.style.visibility = "hidden";
+    this.elemHome.style.visibility = "hidden";
+     this.elemSearch.style.visibility = "hidden"; 
+    this.elemProfile.style.visibility = "hidden";
+    this.elemLogout.style.visibility = "hidden";
+    sessionStorage.clear();
+    */
+  }
+
+}
