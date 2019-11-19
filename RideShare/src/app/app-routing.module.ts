6a17b313a3f9be33a9edbb229f10e2f000b4b603
doriapameni <doriapameni@gmail.com>
2019-11-19 18:58:52 -0500

    adding the Jenkins file to run the pipeline #1

--- RideShare/src/app/app-routing.module.ts
+++ RideShare/src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
 
-const appRoutes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent},
-];
+
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routing = RouterModule.forRoot(appRoutes);
--- RideShare/src/app/app.module.ts
+++ RideShare/src/app/app.module.ts
@@ -1,29 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';	
 
-import { AppRoutingModule, routing } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { NavbarOuterComponent } from './navbar-outer/navbar-outer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegistrationComponent,
-    NavbarOuterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    routing,
-    RouterModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
