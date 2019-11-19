72b5143ca3ff58932d060ce17d0529b8bfbd54f5
Carmichael <carmichael901230@gmail.com>
2019-11-19 09:37:53 -0500

    added dependency for PhantomJS

--- RideShare/karma.conf.js
+++ RideShare/karma.conf.js
@@ -1,32 +1,38 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+
+// process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Project3'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+          require('karma-jasmine'),
+          require('karma-chrome-launcher'),
+          require('karma-phantomjs-launcher'),
+          require('karma-jasmine-html-reporter'),
+          require('karma-coverage-istanbul-reporter'),
+          require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+          clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      coverageIstanbulReporter: {
+          reports: ['html', 'lcovonly'],
+          fixWebpackSourcePaths: true
+      },
+      angularCli: {
+          environment: 'dev'
+      },
+      reporters: ['progress', 'kjhtml'],
+      colors: true,
+      browserDisconnectTimeout : 20000,
+      browserNoActivityTimeout: 20000,
+
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['PhantomJS'],
+      singleRun: true
   });
 };
