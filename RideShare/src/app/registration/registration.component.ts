6f423b48b36fe60d49f85c5443be1f4a62ca72a1
Jeff Franken <50278140+Monjoie11@users.noreply.github.com>
2019-11-25 08:52:41 -0500

    commiting befoe merging in new changes from dev

--- RideShare/src/app/registration/registration.component.ts
+++ RideShare/src/app/registration/registration.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 
-
 /**
  * This component is responsible for registering new users
  */
@@ -12,43 +11,40 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
-  /** 
+  /**
    * The email to be used to log in
    */
-  email: ""
+  email: '';
 
   /**
-   * User's password 
+   * User's password
    */
-  password: ""
+  password: '';
   //fullName: ""
-  
-  /** 
-   * String used for string interpolation 
-   */
-  backtologin = "Back to login";
 
-  /** 
-   * String used for string interpolation 
+  /**
+   * String used for string interpolation
    */
-  register = "register";
+  backtologin = 'Back to login';
+
+  /**
+   * String used for string interpolation
+   */
+  register = 'register';
   response: string;
 
   constructor(
     private route: ActivatedRoute,
-  private router: Router,
-  private http: HttpClient
-  ) { }
-  
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
   elemProfile = document.getElementById('profile');
   elemLogout = document.getElementById('logout');
-    
+
   ngOnInit() {
-    
-    this.elemProfile.style.visibility = "hidden";
-    this.elemLogout.style.visibility = "hidden";
-    
+    this.elemProfile.style.visibility = 'hidden';
+    this.elemLogout.style.visibility = 'hidden';
   }
 
   /**
@@ -61,8 +57,5 @@ export class RegistrationComponent implements OnInit {
   /**
    * Registers user in the system
    */
-  onRegister(): void {
-    
-  }
-  
+  onRegister(): void {}
 }
