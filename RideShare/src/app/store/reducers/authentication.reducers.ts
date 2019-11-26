26c66bc82e2eef6a1cab4106bfec1d7cd806de90
Carmichael <carmichael901230@gmail.com>
2019-11-26 11:40:32 -0500

    added authentication reducers

--- RideShare/src/app/store/reducers/authentication.reducers.ts
+++ RideShare/src/app/store/reducers/authentication.reducers.ts
@@ -1,17 +1,36 @@
-import { 
-    AuthenticationActionTypes,
-    AuthenticationAction
+import {
+  AuthenticationActionTypes,
+  AuthenticationAction
 } from '../actions/authentication.actions';
 
+/**
+ * The initial Authentication token.
+ * If no authentication token is loaded yet, the initial authentication token is used
+ */
 const initialAuthorize = '';
 
+/**
+ * The reducer function which updates the authentication token for current login user,
+ * depending on the type of actions
+ * @param {string} state the current authentication token
+ * @param {AuthenticationAction} action the current action need to be handled
+ */
 export function AuthenticationReducer(
-    state: string,
-    action: AuthenticationAction
+  state: string = initialAuthorize,
+  action: AuthenticationAction
 ) {
-    switch (action.type) {
-        case AuthenticationActionTypes.LOAD_AUTHENTICATION: {
-            state: action
-        }
+  switch (action.type) {
+    case AuthenticationActionTypes.LOAD_AUTHENTICATION: {
+      return {
+        state: action.payload
+      };
     }
+    case AuthenticationActionTypes.AUTHENTICATION_EXPIRED: {
+      return {
+        state: initialAuthorize
+      };
+    }
+    default:
+      return state;
+  }
 }
