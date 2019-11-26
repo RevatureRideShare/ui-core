4807871589bead32bb649dbdc53c7b582bcb710c
Jeff Franken <50278140+Monjoie11@users.noreply.github.com>
2019-11-26 16:03:58 -0500

    #7 psuhing broken code to check ngrx error

--- RideShare/src/app/registration/registration.component.ts
+++ RideShare/src/app/registration/registration.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { UserRegistrationService } from '../services/registration/user-registration.service';
 import { RideStatus } from '../models/user.model';
 import { Role } from '../models/user.model';
 import { User } from '../models/user.model';
 import { HouseLocation } from '../models/houselocation.model';
 import { TrainingLocation } from '../models/traininglocation.model';
 import { Car } from '../models/car.model';
-import {Store} from '@ngrx/store'
+import {Store} from '@ngrx/store';
+import {IAppState} from '../models/states/app-state.model'; 
 
 @Component({
   selector: 'app-registration',
@@ -29,7 +27,7 @@ export class RegistrationComponent implements OnInit {
   accountStatus: boolean;
 
   houseLocation: HouseLocation;
-  housingLoactionName: string;
+  housingLocationName: string;
 
   trainingLocation: TrainingLocation;
   trainingLocationName: String;
@@ -46,10 +44,7 @@ export class RegistrationComponent implements OnInit {
 
 
   constructor(
-    private svc: UserRegistrationService,
-    private router: Router,
-    private http: HttpClient
-    private store: Store<IAppState>
+   private store: Store<IAppState> 
   ) {
     this.car = new Car();
     this.user = new User();
@@ -59,47 +54,9 @@ export class RegistrationComponent implements OnInit {
   ngOnInit() {}
 
   register() {
-    this.result = this.svc.registerUser(
-      this.firstName,
-      this.lastName,
-      this.email,
-      this.password,
-      this.phoneNumber,
-      this.role,
-      this.userCode,
-      this.userBiography,
-      this.workType
-    );
-
-    if (this.result == true) {
-      alert('registration successful');
-      this.router.navigateByUrl('login');
-      return true;
-    } else {
-      alert('registration failed');
-      this.router.navigateByUrl('login');
-      return false;
-    }
+    
   }
 
-  add() {
-    if (this.containers.length < 5) {
-      this.containers.push(this.counter);
-      this.counter++;
-    }
-  }
-
-  remove() {
-    let element = document.getElementsByClassName('company-code');
-    if (this.containers.length > 1) {
-      element[this.containers.length - 1].parentNode.removeChild(
-        element[this.containers.length - 1]
-      );
-      const index = this.containers.indexOf(this.containers.length - 1, 0);
-      if (index > -1) {
-        this.containers.splice(index, 1);
-        this.counter--;
-      }
-    }
-  }
+ 
+    
 }
