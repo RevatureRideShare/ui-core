c461258a13ae295311e22bbc3d073d93333ffc79
Carmichael <carmichael901230@gmail.com>
2019-11-27 12:05:57 -0500

    fix app.module import problem #34

--- RideShare/src/app/app.module.ts
+++ RideShare/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';	
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule, routing } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,12 +13,14 @@ import { RegistrationComponent } from './registration/registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { AllUserReducer } from './store/reducers/all-user.reducers';
-import { AllTrainingReducer, AllHousingReducer } from './store/reducers/location.reducers';
-import { AuthenticationReducer } from './store/reducers/authentication.reducers'
+import {
+  AllTrainingReducer,
+  AllHousingReducer
+} from './store/reducers/location.reducers';
+import { AuthenticationReducer } from './store/reducers/authentication.reducers';
 
 //This is the material toolbar import and associated icon import
-import {MatToolbarModule} from '@angular/material/toolbar';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 //these ng material imports are used in the registration and login components
 import {
@@ -26,12 +28,9 @@ import {
   MatInputModule,
   MatCardModule,
   MatButtonModule
-  
-  } from '@angular/material';
-  import {MatSelectModule} from '@angular/material/select';
-  import {MatIconModule} from '@angular/material/icon';
-import { AuthenticationReducer } from './store/reducers/authentication.reducers';
-
+} from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegistrationComponent],
@@ -50,16 +49,16 @@ import { AuthenticationReducer } from './store/reducers/authentication.reducers'
     MatSelectModule,
     MatToolbarModule,
     MatIconModule,
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production
-    }),
     StoreModule.forRoot({
       allUsers: AllUserReducer,
       allTrainingLocations: AllTrainingReducer,
       allHousingLocations: AllHousingReducer,
       authentication: AuthenticationReducer
     }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
