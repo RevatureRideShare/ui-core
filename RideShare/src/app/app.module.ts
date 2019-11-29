03f043c09c815a1292ae1fb3965792515876f1fb
Carmichael <carmichael901230@gmail.com>
2019-11-29 14:43:05 -0500

    fixed merge problem #34

--- RideShare/src/app/app.module.ts
+++ RideShare/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule, routing } from './app-routing.module';
 import { AppComponent } from './app.component';
--- RideShare/src/app/models/traininglocation.model.ts
+++ RideShare/src/app/models/traininglocation.model.ts
@@ -1,18 +1,19 @@
 /**
  * class TrainingLocation contains any information regarding a known training location.
- *
+ * 
  * Imported by:
  * houseLocation.model.ts
  * location-state.model.ts
- * housing-location.service.ts
  */
-export class TrainingLocation {
+export class TrainingLocation{
   /**
    * trainingLocationName: string - name of the training location
    */
   trainingLocationName: string;
 
-  constructor(trainingLocationName: string = '') {
+  constructor(
+    trainingLocationName: string = "",
+  ){
     this.trainingLocationName = trainingLocationName;
   }
 }
