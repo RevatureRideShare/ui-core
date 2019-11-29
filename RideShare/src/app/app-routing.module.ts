50b3ac89839f4b26eef1958235a7b48b217307ba
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-11-29 15:03:23 -0500

    For issue #42, added HomeComponent to the routing paths.

--- RideShare/src/app/app-routing.module.ts
+++ RideShare/src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent},
+  // { path: '', component: LoginComponent },
+  // { path: 'registration', component: RegistrationComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
 export const routing = RouterModule.forRoot(appRoutes);
