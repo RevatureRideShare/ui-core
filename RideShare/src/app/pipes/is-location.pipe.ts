d717433c684191d00ca1fa39b550df4dbfc8b7a2
Jeff Franken <50278140+Monjoie11@users.noreply.github.com>
2019-11-29 19:25:05 -0500

    #51 pipe running registrtaio is 100 percent

--- RideShare/src/app/pipes/is-location.pipe.ts
+++ RideShare/src/app/pipes/is-location.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { HouseLocation } from '../models/houselocation.model';
 
 @Pipe({
   name: 'isLocation'
 })
 export class IsLocationPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(values: HouseLocation[], locationName: string, pure: false): any {
+    return values.filter((item) => item.trainingLocation.trainingLocationName == locationName);
   }
 
 }
