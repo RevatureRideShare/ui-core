088bc4307276eb77680e934976127124d394a99d
Robert Li <bobli2011@gmail.com>
2019-11-30 11:50:41 -0500

    (style) deleting commented code #20

--- RideShare/src/app/login/login.component.ts
+++ RideShare/src/app/login/login.component.ts
@@ -57,8 +57,5 @@ export class LoginComponent implements OnInit {
     private http: HttpClient
   ) {}
 
-  ngOnInit() {
-    //this.elemProfile.style.visibility = 'hidden';
-    //this.elemLogout.style.visibility = 'hidden';
-  }
+  ngOnInit() {}
 }
