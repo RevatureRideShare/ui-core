5c214e7100a53195ba7f3c641c96faa87fa09b5d
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-12-01 12:52:51 -0500

    For issue #34, removed any unrelated pieces.

--- RideShare/src/app/app.module.ts
+++ RideShare/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppRoutingModule, routing } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -21,10 +19,6 @@ import {
 import { AuthenticationReducer } from './store/reducers/authentication.reducers';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProfileComponent } from './profile/profile.component';
-import { DriverComponent } from './driver/driver.component';
-import { IsLocationPipe } from './pipes/is-location.pipe';
 
 // This is the material toolbar import and associated icon import
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -46,17 +40,7 @@ import { AllUserEffects } from './store/effects/all-user.effects';
 import { RouteEffects } from './store/effects/route.effects';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegistrationComponent,
-    HomeComponent,
-    FooterComponent,
-    NavbarComponent,
-    ProfileComponent,
-    DriverComponent,
-    IsLocationPipe
-  ],
+  declarations: [AppComponent, HomeComponent, FooterComponent],
   imports: [
     BrowserModule,
     routing,
--- RideShare/src/app/pipes/is-location.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsLocationPipe } from './is-location.pipe';
-
-describe('IsLocationPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IsLocationPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
